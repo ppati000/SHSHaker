ba6016749f55aad80da402929bf67e243aa6406c
Patrick Petrovic <ppati000@me.com>
2015-06-26 18:08:51 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: patrick
+ *
+ * Created on June 26, 2015, 6:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
